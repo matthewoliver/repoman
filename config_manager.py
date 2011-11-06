58eb909e9bf2bdac7f64b12c544a262a6513c88f
Matthew Oliver <matt@oliver.net.au>
2011-11-06 19:38:00 +1100

    Finished the config parsing of the config manager.

--- config_manager.py
+++ config_manager.py
@@ -1,9 +1,21 @@
+import ConfigParser
+
 #KEY CONSTANTS
 CONFIG_LOG_FILE = "log_file"
 CONFIG_REPO_PATH = "repo_path"
 CONFIG_WORKING_PATH = "working_path"
 CONFIG_CACHE_PATH = "cache_path"
 CONFIG_SERVER_URL = "server_url" 
+
+DEFAULT_SECTION = "main"
+
+DEFAULT_VALUES = {
+	CONFIG_LOG_FILE : "/var/log/repoman.log",
+	CONFIG_REPO_PATH : "/etc/yum.repos.d/",
+	CONFIG_WORKING_PATH : "var/cache/repoman/working",
+	CONFIG_CACHE_PATH :. "var/cache/repoman/",
+	CONFIG_SERVER_URL : "http://oliver.net.au/repos/",
+}
 
 class config_manager():
 	__config_items=None	
@@ -14,8 +26,21 @@ class config_manager():
 		self.__repoman = repoman
 		self.__config_items=[]
 		self.__config_file=config_file
+		
+		parseConfig()
 
 	
+	def parseConfig(self):
+		config = ConfigParser.ConfigParser(DEFAULT_VALUES)
+		config.read(__config_file)
+		
+		self.__config_items[CONFIG_LOG_FILE] = config.get(DEFAULT_SECTION, CONFIG_LOG_FILE)
+		self.__config_items[CONFIG_REPO_PATH] = config.get(DEFAULT_SECTION, CONFIG_REPO_PATH)
+		self.__config_items[CONFIG_WORKING_PATH] = config.get(DEFAULT_SECTION, CONFIG_WORKING_PATH)
+		self.__config_items[CONFIG_CACHE_PATH] = config.get(DEFAULT_SECTION, CONFIG_CACHE_PATH)
+		self.__config_items[CONFIG_SERVER_URL] = config.get(DEFAULT_SECTION, CONFIG_SERVER_URL)
+		
+
 	def hasConfigItem(self, key):
 		return self.__config_items.__contains__(key)
 
